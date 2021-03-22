bac133c199dcec55308a6f0fd4e8e9084272ea8f
Ayushi Gupta <ayushigupta@Ayushis-MacBook-Air.local>
2021-03-23 00:28:29 +0700

    Added 2 more linked list algorithms

---
+++ src/java/linkedlist/LinkedListQues4.java
@@ -0,0 +1,34 @@
+class LinkedListQues3 {
+    private Node head;
+
+    class Node {
+        int value;
+        Node next;
+    }
+
+    // 1->2->3->4->5->null
+    // null<-1<-2<-3<-4<-5
+    public Node reverseListNonRecursive(Node head) {
+        if (head == null || head.next == null) return head;
+        var prev = head;
+        var start = head.next;
+        while (start.next != null) {
+            var temp = start.next;
+            start.next = prev;
+            prev = start;
+            start = temp;
+        }
+        start.next = prev;
+        head.next = null;
+        return start;
+    }
+
+    public Node reverseList(Node head) {
+        if (head == null || head.next == null) return head;
+        var temp = head.next;
+        head.next = null;
+        var secondElem = reverseList(temp);
+        temp.next = head;
+        return secondElem;
+    }
+}
---
+++ src/java/linkedlist/LinkedListQuestion3.java
@@ -0,0 +1,25 @@
+class LinkedListQues3{
+    private Node head;
+
+    class Node {
+        int value;
+        Node next;
+    }
+
+    // 1->2->3->5->null
+    // insert 4
+    public void insertNodeInSortedList(int value) {
+        var node = new Node(value);
+        if (this.head == null) {
+            this.head = node;
+        }
+        var start = this.head;
+        var prev = this.head;
+        while (start != null && start.value <= value) {
+            prev = start;
+            start = start.next;
+        }
+        node.next = start;
+        prev.next = node;
+    }
+}
